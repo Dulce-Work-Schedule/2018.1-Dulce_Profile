746443ad275ec7379ed9ce50ebde0001f9e71326
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-05-29 12:05:06 -0300

    Adicionando script de deploy ao travis
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>

--- Client/client_user.js
+++ Client/client_user.js
@@ -14,50 +14,50 @@ jwtOptions.jwtFromRequest = PassportJwt.ExtractJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = '123456789'
 
 var strategy = new JwtStrategy(jwtOptions, async function(payload, next) {
-      console.log('payload received', payload)
-      console.log(next)
-      next(null, payload)
+    console.log('payload received', payload)
+    console.log(next)
+    next(null, payload)
 })
 
 Passport.use(strategy)
 
 Passport.serializeUser((user, cb) => {
-      cb(null, user)
-  })
+    cb(null, user)
+})
 
-  Passport.deserializeUser((user, cb) => {
-      cb(null, user)
-  })
+Passport.deserializeUser((user, cb) => {
+    cb(null, user)
+})
 
 
 var app = express()
-      .use( require('body-parser').json() )
-      .use(Passport.initialize())
-      .use( context )
+    .use(require('body-parser').json())
+    .use(Passport.initialize())
+    .use(context)
 
 var senecaWebConfig = {
-      context: context,
-      adapter: require('seneca-web-adapter-express'),
-      options: { parseBody: false },
-      auth: Passport
+    context: context,
+    adapter: require('seneca-web-adapter-express'),
+    options: { parseBody: false },
+    auth: Passport
 }
 
 var seneca = require('seneca')()
-      .use(SenecaWeb, senecaWebConfig )
-      .use('seneca-amqp-transport')
-      .use("entity")
-      .use('api')
-      .client( {
-          type:'amqp',
-          pin:'role:user',
-          port: process.env.RABBITMQ_PORT,
-          username: process.env.RABBITMQ_DEFAULT_USER,
-          password: process.env.RABBITMQ_DEFAULT_PASS,
-          url: 'amqp://' + process.env.RABBITMQ_HOST
-         } )
-      .ready(() => {
-            console.log('here eye am')
-            app.listen(8080)
-      })
-      
+    .use(SenecaWeb, senecaWebConfig)
+    .use('seneca-amqp-transport')
+    .use("entity")
+    .use('api')
+    .client({
+        type: 'amqp',
+        pin: 'role:user',
+        port: process.env.RABBITMQ_PORT,
+        username: process.env.RABBITMQ_DEFAULT_USER,
+        password: process.env.RABBITMQ_DEFAULT_PASS,
+        url: 'amqp://' + process.env.RABBITMQ_HOST
+    })
+    .ready(() => {
+        console.log('Manga')
+        app.listen(8080)
+    })
+
 module.exports = app
