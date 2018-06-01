9f84db8cf0b3ad51d598f9a887d5314928d7dcab
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-06-01 12:22:02 -0300

    Fix#38 Mudando ambiente de dev no rancher para Production

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -7,5 +7,5 @@ sudo docker-compose -f build.yml build
 sudo docker login --username=$DOCKERHUB_USER --password=$DOCKERHUB_PASSWORD
 sudo docker push dulce/user-client:prod
 sudo docker push dulce/user-server:prod
-sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Dev --stack User --service userclient --finish-upgrade 
-sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Dev --stack User --service userclient --finish-upgrade 
+sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack User --service userclient --finish-upgrade 
+sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack User --service userclient --finish-upgrade 
